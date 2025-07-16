e7b71cbca6946f039af683ffafd6925982376378
aleena1794 <aleena.george180@gmail.com>
2025-07-16 17:36:19 +0200

    Update hpo_nn.py

--- src/models/hpo_nn.py
+++ src/models/hpo_nn.py
@@ -44,9 +44,9 @@ def run_nn_kfold_with_hpo(X_train, y_train, X_val, X_test, y_val, y_test, k=5):
     #final_model = build_nn(X_train.shape[1], best_params['n_units_1'], best_params['n_units_2'])
     #final_model.fit(X_train, y_train, epochs=best_params['epochs'], batch_size=best_params['batch_size'],
                     #verbose=0, callbacks=[EarlyStopping(monitor='val_mae', patience=3, restore_best_weights=True)])
-    final_model = train_final_model(X_train, y_train, X_val, y_val, X_train.shape[1], best_params)
+    final_model,history = train_final_model(X_train, y_train, X_val, y_val, X_train.shape[1], best_params)
 
     y_pred_val = final_model.predict(X_val).flatten()
     y_pred_test = final_model.predict(X_test).flatten()
 
-    return y_pred_val, y_pred_test, best_params, best_mae
+    return y_pred_val, y_pred_test, best_params, best_mae,history
